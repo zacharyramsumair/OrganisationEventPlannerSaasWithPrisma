f10fedc0e56dacdea0694bb5b9891fe76fdf2c08
Zachary <ramsumairzachary@gmail.com>
2024-07-02 12:15:45 -0400

    basic dashboard works

--- actions/organisation.ts
+++ actions/organisation.ts
@@ -15,34 +15,26 @@ const isOrganisationUsernameUnique = async (username: string) => {
 const createOrganisation = async (formData: any, currentUser: any) => {
   const { name, description, contactNumber, email, organisationMainUser, username } = formData;
 
-  console.log("here organ", organisationMainUser)
-console.log("here 1")
   try {
     // Check if the organisation username is unique
     const isUnique = await isOrganisationUsernameUnique(username);
-    console.log("here 1.5")
 
     if (!currentUser) {
-        console.log("here 2")
       throw new Error("Not Authenticated");
     }
 
     if (!isUnique) {
-        console.log("here 2")
       throw new Error("Organisation username is already taken. Please choose a different username.");
     }
 
     // let currentUser = await getCurrentUser();
-    console.log("here 2.5")
 
 
     if (currentUser?.organisations?.length > 0) {
-        console.log("here 3")
       throw new Error("Only allowed One Organisation per account. Upgrade to create more");
     }
 
 
-    console.log("here 3.5")
 
     const newOrganisation = await prisma.organisation.create({
       data: {
@@ -56,7 +48,6 @@ console.log("here 1")
     });
 
 
-    console.log("here 4")
     // Update the current user's organisations array
     await prisma.user.update({
       where: { id: currentUser.id },
@@ -67,21 +58,21 @@ console.log("here 1")
       },
     });
 
-    console.log("here 5")
 
     return newOrganisation;
   } catch (error: any) {
 
-    console.log("here 6")
     console.log(error.message);
     throw new Error("Error while creating Organisation");
   }
 };
 
-const getOrganisationById = async (organisationId: string) => {
+const getOrganisationById = async (organisationId: any) => {
+
   try {
+
     const organisation = await prisma.organisation.findUnique({
-      where: { id: organisationId },
+      where: { id: organisationId.id },
       include: {
         events: {
           orderBy: { updatedAt: "desc" },
@@ -89,13 +80,14 @@ const getOrganisationById = async (organisationId: string) => {
       },
     });
 
+
+
     if (!organisation) {
       throw new Error("Organisation not found");
     }
 
     return organisation;
   } catch (error: any) {
-    console.log("here we are", error.message);
     throw new Error("Error while fetching Organisation");
   }
 };
---
+++ app/(organisation)/dashboard/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { redirect } from "next/navigation";
+import Dashboard from '@/components/Dashboard';
+// import { fetchOrganisationById } from '@/app/api/organisation/getOrganisationById/route';
+// import { fetchCurrentUser } from "@/app/api/user/getCurrentUser/route";
+import { getUserByEmail } from "@/actions/auth";
+import { auth } from "@/auth";
+import { getOrganisationById } from '@/actions/organisation';
+
+
+type Props = {}
+
+const dashboard = async (props: Props) => {
+    const session = await auth();
+
+	let currentUser: any = false;
+	if (session?.user?.email) {
+		currentUser = await getUserByEmail(session?.user?.email);
+	}
+
+
+	if (!currentUser) {
+		redirect("/login");
+	}
+
+    if (!(currentUser?.organisations?.length > 0)) {
+		redirect("/createOrganisation");
+	}
+
+
+	
+
+  let organisationInformation = await getOrganisationById(currentUser?.organisations[0])
+  console.log(organisationInformation)
+
+  
+  return (
+    <div>
+      
+      <Dashboard organisationInformation={organisationInformation}/>
+    </div>
+  )
+}
+
+export default dashboard
