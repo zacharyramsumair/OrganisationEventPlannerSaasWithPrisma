db50e0a25903e6b25ee546d754fdc7297c121483
Zachary <ramsumairzachary@gmail.com>
2024-07-05 14:06:35 -0400

    no demo vid

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -81,7 +81,7 @@ function HomePage({ currentUser }: any) {
 					)}
 
 					<a
-						href="https://www.loom.com/share/91487fbd56954a69aa304ff2382545ae?sid=8288cb22-87f4-4784-9624-7b43459bc9c1"
+						href="#"
 						className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
 					>
 						<svg
